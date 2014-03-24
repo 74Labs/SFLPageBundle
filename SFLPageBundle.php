4f6a56d99b6f6cfaf67ab2ad9f38e8c902920839
trace <seventy.four.laboratories@gmail.com>
2014-03-24 11:51:25 +0100

    Revert "Added bundle code and PHPUnit config"
    This reverts commit edb625b1c9e73fe395c790646702d3b895c6b754.

--- SFLPageBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace SFL\PageBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class SFLPageBundle extends Bundle
-{
-}
--- Tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$file = __DIR__.'/../vendor/autoload.php';
-if (!file_exists($file)) {
-    throw new RuntimeException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
-}
-
-require_once $file;
